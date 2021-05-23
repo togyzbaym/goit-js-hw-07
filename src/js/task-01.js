650dede29b2c9d22734c42a83bfa7631305d068a
togyzbaym <mokernamew@gmail.com>
2021-05-23 17:59:20 +0300

    changed some codes

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,13 +1,7 @@
-// const listItem = document.querySelectorAll('.item');
-// console.log(listItem);
-const titles = document.querySelectorAll('.list-itme-title');
-const listInlist = document.querySelectorAll('.list-item-list');
 
-console.log(`Категория:${titles[0].textContent}`);
-console.log(`Количество элементов:${listInlist[0].children.length}`);
-
-console.log(`Категория:${titles[1].textContent}`);
-console.log(`Количество элементов:${listInlist[1].children.length}`);
-
-console.log(`Категория:${titles[2].textContent}`);
-console.log(`Количество элементов:${listInlist[2].children.length}`);
+const myItem = document.querySelectorAll('.list-item');
+const result = myItem.forEach(el => {
+    console.log(`Категория: ${el.firstElementChild.textContent}`);
+    console.log(`Количество элементов: ${el.lastElementChild.children.length}`);
+});
+   

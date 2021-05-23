3276bdbd33733b3fdc8d16f79d66bb8cff207268
togyzbaym <mokernamew@gmail.com>
2021-05-23 18:04:25 +0300

    1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,6 @@
 
 const myItem = document.querySelectorAll('.list-item');
-const result = myItem.forEach(el => {
+ myItem.forEach(el => {
     console.log(`Категория: ${el.firstElementChild.textContent}`);
     console.log(`Количество элементов: ${el.lastElementChild.children.length}`);
 });
--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,10 +2,10 @@ const refs = {
     fontSizeControlInput: document.querySelector('#font-size-control'),
     textLable:document.querySelector('#text'),
 }
+
 refs.fontSizeControlInput.addEventListener('input', onChangeFontSize);
 
 function onChangeFontSize() {
     let val = refs.fontSizeControlInput.value;
-
     refs.textLable.style.fontSize = val+'%';
 }
